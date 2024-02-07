9c1832e1c2c3e5881310031f42b04111ea826b12
Pablo Garcia <pablo.garcia366@gmail.com>
2024-02-06 22:23:44 -0300

    Refactor server.go to handle multiple connections

--- app/server.go
+++ app/server.go
@@ -15,13 +15,15 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	handleCoon(conn)
+		handleCoon(conn)
+	}
 }
 
 func handleCoon(c net.Conn) error {
